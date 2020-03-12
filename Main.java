9294d4ce39f28fdf228a3023a327162bb4b23932
kevinandres85 <62110811+kevinandres85@users.noreply.github.com>
2020-03-12 11:07:44 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main{
+	public void rama1(){
+System.out.print("soy la rama1");
+}
+public void rama2(){
+System.out.print("soy la rama2");
+}
+public void rama3(){
+System.out.print("soy la rama3");
+}
+
+public void rama4(){
+System.out.print("soy la rama4");
+}
+
+public void rama5(){
+System.out.print("soy la rama5");
+}
+
+}
